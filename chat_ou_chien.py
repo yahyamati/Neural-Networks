64f853e5ad63b07da8529a65325be667f973843c
Yahia Mati <yahyamati8@gmail.com>
2025-03-01 01:43:43 +0100

    change log loss function to log_loss from sklearn.metrics && train the model on train_set

--- chat_ou_chien.py
+++ chat_ou_chien.py
@@ -1,89 +1,114 @@
 from utilities import *
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
-# from sklearn.datasets import make_blobs
-# from sklearn.metrics import accuracy_score
+from sklearn.datasets import make_blobs
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+
+
+X_train , y_train , X_test , y_test = load_data()
+
+#normalize the train and test sets (scaling pixel values from 0-255 to 0-1)
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+print("Train set min:", X_train.min(), "max:", X_train.max())
+print("Test set min:", X_test.min(), "max:", X_test.max())
 
 
 
+X_train = X_train.reshape(X_train.shape[0], -1)  # Shape (num_samples, 4096)
+X_test = X_test.reshape(X_test.shape[0], -1)  # Shape (num_samples, 4096)
+
+print("X_train shape after flattening:", X_train.shape)
+print("X_test shape after flattening:", X_test.shape)
 
 
-# X,y = make_blobs(n_samples = 100,n_features = 5,centers = 2 , random_state=0)
-# y=y.reshape((y.shape[0],1))
+y_train =y_train.flatten()
+y_test = y_test.flatten()
+print("y_train shape after flattening:", y_train.shape)
+print("y_test shape after flattening:", y_test.shape)
+
+# plt.figure(figsize=(16,8))
+# for i in range(1,10):
+#     plt.subplot(4,5,i)
+#     plt.imshow(X_train[i],cmap='gray')
+#     plt.title(y_train[i])
+#     plt.tight_layout()
+# plt.show()
 
 
-# # print('demension de X',X.shape)
-# # print('demension de y',y.shape)
 
-# # plt.scatter(X[:,0] , X[:,1] ,c=y ,cmap='summer')
-# # plt.show()
-
-
-
-
-# def initialisation(X):
-#     W = np.random.randn(X.shape[1],1)
-#     b = np.random.randn(1)
+def initialisation(X_train):
+    W = np.random.randn(X_train.shape[1],1)
+    b = np.random.randn(1)
     
-#     return(W,b)
+    return(W,b)
 
 
-# def Model(X,W,b): 
-#     Z = np.dot(X , W) + b
-#     A = 1/(1+np.exp(-Z))
+def Model(X_train,W,b): 
+    Z = np.dot(X_train , W) + b
+    A = 1/(1+np.exp(-Z))
     
-#     return A
+    return A
 
 
-# def log_loss(A,y):
-#     return 1/len(y) * np.sum(-y * np.log(A) - (1-y) * np.log(1-A))
+# def log_loss(A,y_train):
+#     return 1/len(y_train) * np.sum(-y_train * np.log(A) - (1-y_train) * np.log(1-A))
 
 
-# def gradient(A,X,y):
-#     dw = 1/len(y) * np.dot(X.T , A-y)
-#     db = 1/len(y) * np.sum(A-y)
-#     return (dw , db)
+def gradient(A,X_train,y_train):
+    dw = 1/len(y_train) * np.dot(X_train.T , A-y_train)
+    db = 1/len(y_train) * np.sum(A-y_train)
+    return (dw , db)
 
 
-# def update(W,b,dw,db,learning_rate):
-#     W = W - learning_rate * dw
-#     b = b - learning_rate * db
+def update(W,b,dw,db,learning_rate):
+    W = W - learning_rate * dw
+    b = b - learning_rate * db
     
-#     return (W,b)
+    return (W,b)
 
 
 
-# def predict(X,W,b):
-#     A = Model(X,W,b)
-#     print("predict point==>",A)
-#     return A>=0.5
+def predict(X_train,W,b):
+    A = Model(X_train,W,b)
+    print("predict point==>",A)
+    return A>=0.5
     
 
 
-# def artificial_neuron(X,y,learning_rate=0.1,nb_iteration = 100):
-#     #initialisation
-#     W,b = initialisation(X)
+def artificial_neuron(X_train,y_train,learning_rate=0.1,nb_iteration = 1000):
+    y_train = y_train.reshape(-1, 1)
+    #initialisation
+    W,b = initialisation(X_train)
     
-#     loss = []
-#     for i in range(nb_iteration):
-#         A = Model(X,W,b)
-#         loss.append(log_loss(A,y))
-#         dw,db =gradient(A,X,y)
-#         #update
-#         W,b = update(W,b,dw,db,learning_rate)
+    loss = []
+    for i in range(nb_iteration):
+        A = Model(X_train,W,b)
         
-#     y_pred = predict(X,W,b)
-#     print(f"accuracy_score=>{accuracy_score(y,y_pred)*100}%")
+        print("A==>",A.shape)
+        print("y_train==>",y_train.shape)
+        loss.append(log_loss(y_train, A))
+        dw,db =gradient(A,X_train,y_train)
+        #update
+        W,b = update(W,b,dw,db,learning_rate)
+        
+    y_pred = predict(X_train,W,b)
+    print(f"accuracy_score=>{accuracy_score(y_train,y_pred)*100}%")
         
     
-#     # plt.plot(loss)
-#     # plt.show()
-#     return (W,b)
+    plt.plot(loss)
+    plt.xlabel("Iterations")
+    plt.ylabel("Log Loss")
+    plt.title("Loss Curve")
+    plt.show()
+    return (W,b)
     
     
-# W,b = artificial_neuron(X,y) 
-# print("the best w=>",W)
-# print("the best b=>",b)   
+W,b = artificial_neuron(X_train,y_train) 
+print("the best w=>",W)
+print("the best b=>",b)   
 
 
 
@@ -114,20 +139,5 @@ import matplotlib.pyplot as plt
 
 
 
-X_train , y_train , X_test , y_test = load_data()
 
-#normalize the train and test sets (scaling pixel values from 0-255 to 0-1)
-X_train = X_train / 255.0
-X_test = X_test / 255.0
-
-print("Train set min:", X_train.min(), "max:", X_train.max())
-print("Test set min:", X_test.min(), "max:", X_test.max())
-
-plt.figure(figsize=(16,8))
-for i in range(1,10):
-    plt.subplot(4,5,i)
-    plt.imshow(X_train[i],cmap='gray')
-    plt.title(y_train[i])
-    plt.tight_layout()
-plt.show()
 
